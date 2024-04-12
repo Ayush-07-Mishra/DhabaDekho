f27f9b83ffc1124d30c308ea5926505f483ebb83
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-12 21:43:16 +0530

    dhabadekho

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='fixed z-[999] w-full px-[4vw] py-[2vw] font-["Neue Montreal"] flex justify-between items-center'>
+        <div className='Logo font-semibold text-[2vw]'>DhabaDekho</div>
+    
+        <div className='links flex gap-10'>
+            {["Services","About us","Contact us"].map((item,index)=>(
+                <a key={index} className={`text-lg capatalize font-light ${index ===2 && "ml-56"}`}>{item}</a>
+            ))}
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
