8d80c6f96e1ad8f028b1ca19941ec33949ce3fcd
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-17 22:50:45 +0530

    e

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { GrSearch } from "react-icons/gr";
 
 function Landing() {
   return (
     <div className="w-full  bg-#F1F1F1 pt-1 ">
-      <div className="w-full flex justify-center items-top mt-[6vw] ">
+      <div className="w-full flex justify-center items-center mt-[6vw] ">
         <input
           type="text"
           placeholder="Search"
-          className="border-2 w-[35vw] rounded-[1vw] py-2 px-3 mt-[2vw] border-black"
+          className="border-2 w-[35vw] rounded-[1vw] py-2 px-3 mt-[5vw] border-black"
         />
+        <button className="text-[30px] p-[1vw] mt-20 ">
+          <GrSearch />
+        </button>
       </div>
       <div className="flex justify-between items-center">
         <div className="textstructure mt-[6vw] px-20">
@@ -21,11 +25,14 @@ function Landing() {
               <div className="masker">
                 <div className="w-fit flex items-center">
                   {index === 1 && (
-                    <div className="w-[10vw] h-[9vw] py-[1vw] rounded-full"><img
-                      src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    /></div>
+                    <div className="w-[10vw] h-[9vw] py-[1vw] ">
+                      <img
+                        className="rounded-2xl"
+                        src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      />
+                    </div>
                   )}
-                  <div className='text-[4vw] uppercase leading-[6.5vw] tracking-tighter font-["Avenir_Next"] font-semibold'>
+                  <div className='text-[4.5vw] uppercase leading-[6.5vw] tracking-tight font-["Founder Grotesk Condensed"] font-medium'>
                     <h1>{item}</h1>
                   </div>
                 </div>
@@ -35,6 +42,7 @@ function Landing() {
         </div>
         <div className=" w-[18vw] mr-[6vw]">
           <img
+            className="rounded-2xl "
             src="https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?q=80&w=2580&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Your Image Description"
           />
